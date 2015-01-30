e938bad50acd8c1d1bf3f25ab61b39ebfc65f3bf
Uwe Trottmann <uwe.trottmann@gmail.com>
2015-01-30 13:10:19 +0100

    Use explicit intent when binding to license service.

--- SeriesGuide/src/main/java/com/battlelancer/seriesguide/licensing/LicenseChecker.java
+++ SeriesGuide/src/main/java/com/battlelancer/seriesguide/licensing/LicenseChecker.java
@@ -140,12 +140,11 @@ public class LicenseChecker implements ServiceConnection {
             if (mService == null) {
                 Log.i(TAG, "Binding to licensing service.");
                 try {
+                    Intent intent = new Intent(new String(Base64.decode(
+                            "Y29tLmFuZHJvaWQudmVuZGluZy5saWNlbnNpbmcuSUxpY2Vuc2luZ1NlcnZpY2U=")));
+                    intent.setPackage("com.android.vending");
                     boolean bindResult = mContext
-                            .bindService(
-                                    new Intent(
-                                            new String(
-                                                    Base64.decode(
-                                                            "Y29tLmFuZHJvaWQudmVuZGluZy5saWNlbnNpbmcuSUxpY2Vuc2luZ1NlcnZpY2U="))),
+                            .bindService(intent,
                                     this, // ServiceConnection.
                                     Context.BIND_AUTO_CREATE);
 
